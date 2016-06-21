b05836d7abe7e9084aea2c50bd5ff26445309150
forgere <573596314@qq.com>
2016-06-21 14:35:59 +0800

    3d yanshao

--- js/getromote_jsonp.js
+++ js/getromote_jsonp.js
@@ -12,36 +12,56 @@ $(function() {
 		success: function(result){
 			$('.gr-caption-close').html(result.name);
 			var ajax = result;
-			var ajaxphotos = ajax.scenes,
-					// 每面墙的容量
-					ajaxlayout = [],
-					//图片id
-					photosId = [],
-					//根据id获取到的src数组；
-					photosSrc = [],
-					//描述
-					photosDes = [];
-			//title
-			$('title').html(result.name);
-			$.each(ajaxphotos,function(i) {
-				ajaxlayout[i] = ajaxphotos[i].scene_template.capacity;
-				photosId.push.apply( photosId, ajaxphotos[i].photo_seq.split(","));
-				$.each(photosId,function(j) {
-					$.each(ajax.photos,function(k) {
-						if(ajax.photos[k].id == photosId[j]){
-							photosSrc[j] = "http://kadaphoto.img-cn-beijing.aliyuncs.com/User/" + ajax.photos[k].image + "@80p";
-							photosDes[j] = ajax.photos[k].description;
-						}
+			if(ajax.type_kbn){
+				var photosyanshao = [],photoDesyanshao = [];
+				$.each(result.photos,function(i) {
+					photosyanshao[i] ="http://kadaphoto.img-cn-beijing.aliyuncs.com/User/" + result.photos[i].image;
+					photoDesyanshao[i] = result.photos[i].description;
+				});
+				var ajaxlayoutyanshao=[];
+				console.log(ajax.photos)
+				$.each(ajax.photos,function(i) {
+					ajaxlayoutyanshao[i] = 1;
+				});
+				Gallery.init({
+					layout : ajaxlayoutyanshao,  //给主墙限定一个宽度是为了将图片限制在“墙”的范围内。每一面墙上的图片的数量是图片总数除以wallNumber(最后一面墙是剩下的图片)，我们也可以自定义每面墙上的图片数量(当图片总数不够填补最后一页时，有多少显示多少，没有则空白墙)
+	        romoteObject : photosyanshao,// 图片数组
+	        romoteDes : photoDesyanshao,// 图片描述
+	        name: result.name,
+	        created: result.created.split("T")[0] + " " + result.created.split("T")[1].split(".")[0]
+				});
+			}else{
+				var ajaxphotos = ajax.scenes,
+						// 每面墙的容量
+						ajaxlayout = [],
+						//图片id
+						photosId = [],
+						//根据id获取到的src数组；
+						photosSrc = [],
+						//描述
+						photosDes = [];
+				//title
+				$('title').html(result.name);
+				$.each(ajaxphotos,function(i) {
+					ajaxlayout[i] = ajaxphotos[i].scene_template.capacity;
+					photosId.push.apply( photosId, ajaxphotos[i].photo_seq.split(","));
+					$.each(photosId,function(j) {
+						$.each(ajax.photos,function(k) {
+							if(ajax.photos[k].id == photosId[j]){
+								photosSrc[j] = "http://kadaphoto.img-cn-beijing.aliyuncs.com/User/" + ajax.photos[k].image + "@80p";
+								photosDes[j] = ajax.photos[k].description;
+							}
+						});
 					});
 				});
-			});
-			Gallery.init({
-				layout : ajaxlayout,  //给主墙限定一个宽度是为了将图片限制在“墙”的范围内。每一面墙上的图片的数量是图片总数除以wallNumber(最后一面墙是剩下的图片)，我们也可以自定义每面墙上的图片数量(当图片总数不够填补最后一页时，有多少显示多少，没有则空白墙)
-        romoteObject : photosSrc,// 图片数组
-        romoteDes : photosDes,// 图片描述
-        name: result.name,
-        created: result.created.split("T")[0] + " " + result.created.split("T")[1].split(".")[0]
-			});
+				Gallery.init({
+					layout : ajaxlayout,  //给主墙限定一个宽度是为了将图片限制在“墙”的范围内。每一面墙上的图片的数量是图片总数除以wallNumber(最后一面墙是剩下的图片)，我们也可以自定义每面墙上的图片数量(当图片总数不够填补最后一页时，有多少显示多少，没有则空白墙)
+	        romoteObject : photosSrc,// 图片数组
+	        romoteDes : photosDes,// 图片描述
+	        name: result.name,
+	        created: result.created.split("T")[0] + " " + result.created.split("T")[1].split(".")[0]
+				});
+			}
 		}
 	});
 });
